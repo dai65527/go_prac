9d56c39711e543d91a2a8861fa6f334b08a8dfa1
dnakano <dnakano@student.42tokyo.jp>
2021-03-22 17:45:40 +0900

    update mycat (able to specify option)

--- mycat/mycat.go
+++ mycat/mycat.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
+
+var nFlg = flag.Bool("n", false, "display line number")
 
 func displayFile(filename string, lineNo *int) {
 	fs, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		fs.Close()
 		return
 	}
 
 	scanner := bufio.NewScanner(fs)
 	for scanner.Scan() {
-		fmt.Printf("%4d: %s\n", *lineNo, scanner.Text())
+		if *nFlg {
+			fmt.Printf("%4d: ", *lineNo)
+		}
+		fmt.Printf("%s\n", scanner.Text())
 		*lineNo++
 	}
+
+	fs.Close()
 }
 
 func main() {
+	flag.Parse()
 	lineNo := 1
-	for i := 1; i < len(os.Args); i++ {
-		displayFile(os.Args[i], &lineNo)
+	fmt.Println(flag.Args())
+	for i := 0; i < len(flag.Args()); i++ {
+		displayFile(flag.Args()[i], &lineNo)
 	}
 }
